8de225debc41f101b5e5a4a226e22477d2b5bacf
Shahbaz5523 <shahbaz.rajpoot15@gmail.com>
2021-03-04 13:06:01 +0500

    Get Data from api and display it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Header from './components/Header';
+import MainGrid from './components/MainGrid';
+import CountryContext from './components/CountryContext/CountryContext';
 
 function App() {
+  let initialContext = useState('Global');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <CountryContext.Provider value={initialContext}>
+        <div className="App">
+            <Header/> 
+            <br/>
+            <MainGrid/>
+        </div>
+      </CountryContext.Provider>
   );
 }
 
